b994d9f5fcf48caa9b9f13b30d446fb299c1790d
anyepeter <petzyrockchendi@gmail.com>
2023-10-16 12:17:05 +0100

    add user authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homePage';
-
+import Profile from './pages/profile';
 function App() {
   return (
     <>
-        <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+        
      
     </>
   );
